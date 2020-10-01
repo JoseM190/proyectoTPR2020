ef9ac024c43a8f8a3daf2d3a547088f4fcbd075f
Jose Arturo <artu.mar1903@gmail.com>
2020-10-01 14:21:02 -0400

    agregado

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>LARAVEL</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                <h6>BIENVENIDOS A LA MATERIA DE <br>TECNOLOGIA DE PROGRAMACION EN RED </h6>
                 </div>
 
                 <div class="links">
